96f83a14442fad8036b2495f93b02a229eb2f0e1
Nasir-afridi <nasirwc05@gmail.com>
2025-02-13 14:57:26 +0500

    build the Fully responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark w-full">
+        <div className="container-fluid w-full">
+          <img src="public/portfolio.png" className="portfolio-img" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav gap-4">
+              <li className="nav-item">
+                <a className="nav-link navbar" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navbar" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navbar" href="#">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navbar" href="#">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link navbar" href="#">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default App;
