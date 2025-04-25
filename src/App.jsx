7a6afd6da9885004fb58a3835333e1ebc9ffe680
Nasir-afridi <nasirwc05@gmail.com>
2025-04-25 16:36:32 +0500

    updated the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navbar from './components/pages/nav/Navbar';
+import React from "react";
+import Navbar from "./components/nav/Navbar.jsx";
 
 function App() {
   return (
