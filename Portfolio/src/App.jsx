1123f66db0d2fd97b0c2a6a17ff6731854902348
Nasir-afridi <nasirwc05@gmail.com>
2026-02-06 00:46:45 +0500

    added the Route to the pages

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,24 +1,27 @@
-import Navbar from './components/Navbar/Header'
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Project from "./pages/Projects"
-import Technologies from "./pages/Technologies"
-import Services from "./pages/Services"
-import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Projects";
+import Technologies from "./pages/Technologies";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Navbar />
-      <Home />
-      <About />
-      <Project />
-      <Technologies />
-      <Services />
-      <Contact />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/technologies" element={<Technologies />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
