adf3ba8ec1a821b3f5a7803997c6fee758917362
Nasir-afridi <nasirwc05@gmail.com>
2026-02-05 23:21:33 +0500

    import all the pages and the Navbar also

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './components/Navbar/Header'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Project from "./pages/Projects"
+import Technologies from "./pages/Technologies"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Project />
+      <Technologies />
+      <Services />
+      <Contact />
+    </>
+  )
+}
+
+export default App
